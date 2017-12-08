e6a05086f8517e90c79f464c8da583924f6fc8bb
Aaron M Green <txhc010888@gmail.com>
2017-12-07 19:14:21 -0600

    CP13 work added play and pause button that stayed in place only on single cell

--- scripts/album.js
+++ scripts/album.js
@@ -87,31 +87,71 @@ var getSongItem = function(songEle) {
     case 'song-item-title':
     case 'song-item-duration':
       return findParentByClassName(songEle, 'album-view-song-name').querySelector('.song-item-number');
+
     case 'song-item-number':
       return songEle;
+
     default:
       return;
   }
 };
 
+var clickHandler = function(targetElement) {
+  var songItem = getSongItem(targetElement);
+
+  if (currentlyPlayingSong === null) {
+    songItem.innerHTML = pauseButtonTemplate;
+    currentlyPlayingSong = songItem.getAttribute('data-song-number');
+  }
+  else if (currentlyPlayingSong === songItem.getAttribute('data-song-number')) {
+    songItem.innerHTML = playButtonTemplate;
+    currentlyPlayingSong = null;
+  }
+  else if (currentlyPlayingSong !== songItem.getAttribute('data-song-number')) {
+    var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
+    currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
+    songItem.innerHTML = pauseButtonTemplate;
+    currentlyPlayingSong = songItem.getAttribute('data-song-number');
+  }
+};
+
+//Elements to which we will be adding listeners
 var songListContainer = document.getElementsByClassName("album-view-song-list")[0];
 var songRows = document.getElementsByClassName("album-view-song-item");
 
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+
+//store state of playing songs
+var currentlyPlayingSong = null; //null so no songs are displayed unless one is playing
 
 window.onload = function() {
   setCurrentAlbum(albumPicasso); //is this where the album function is now being told to use the albumPicasso object for the fuction(album) code?
 
   songListContainer.addEventListener("mouseover", function(event) {
     if (event.target.parentElement.className === "album-view-song-item") {
-      event.target.parentElement.querySelector(".song-item-number").innerHTML = playButtonTemplate; //does querySelector use the "." before each class unlike getElementsByClassName??
+      event.target.parentElement.querySelector(".song-item-number").innerHTML = playButtonTemplate;
+
+      var songItem = getSongItem(event.target);
+      if (songItem.getAttribute('data-song-number') !== currentlyPlayingSong) {
+        songItem.innerHTML = playButtonTemplate;
+      }
     }
   });
 
   for (var i = 0; i < songRows.length; i++){
     songRows[i].addEventListener("mouseleave", function(event) {
-      //selects first child element which is the song-item-number element
-      this.children[0].innerHTML = this.children[0].getAttribute("data-song-number");
+      var songItem = getSongItem(event.target);
+      var songItemNumber = songItem.getAttribute('data-song-number');
+
+      if (songItemNumber !== currentlyPlayingSong) {
+        songItem.innerHTML = songItemNumber;
+      }
+    });
+
+    songRows[i].addEventListener('click', function(event) {
+      //Event handler call
+      clickHandler(event.target);
     });
   }
 };
