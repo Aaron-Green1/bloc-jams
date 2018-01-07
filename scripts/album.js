fb3cd8774452c1dca2a744cd282cb9dbb1e6c94b
Aaron M Green <txhc010888@gmail.com>
2018-01-07 11:04:18 -0600

    added nextsong lastsong and indexof functions attempted to enforce consistent data types.  Still getting error.   Working it out.

--- scripts/album.js
+++ scripts/album.js
@@ -9,11 +9,11 @@ var createSongRow = function(songNumber, songName, songLength) {
   var $row = $(template);
 
   var clickHandler = function() {
-    var songNumber = $(this).attr("data-song-number");
+    var songNumber = parseInt($(this).attr("data-song-number"));
 
     if(currentlyPlayingSongNumber !== null){
       //Revert to song number for currently playing song because user started new song
-      var currentlyPlayingSongElement = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+      var currentlyPlayingSongElement = parseInt($('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]'));
       currentlyPlayingSongElement.html(currentlyPlayingSongNumber);
     }
     if(currentlyPlayingSongNumber !== songNumber){
@@ -21,9 +21,11 @@ var createSongRow = function(songNumber, songName, songLength) {
       $(this).html(pauseButtonTemplate);
       currentlyPlayingSongNumber = songNumber;
       currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+      updatePlayerBarSong();
     } else if(currentlyPlayingSongNumber === songNumber){
       //Switch from Pause -> Play button to pause currently playing song.
         $(this).html(playButtonTemplate);
+        $('.main-controls .play-pause').html(playerBarPlayButton);
         currentlyPlayingSongNumber = null;
         currentSongFromAlbum = null;
     }
@@ -31,7 +33,7 @@ var createSongRow = function(songNumber, songName, songLength) {
 
   var onHover = function(event) {
     var songNumberCol = $(this).find(".song-item-number");
-    var songNumber = songNumberCol.attr("data-song-number");
+    var songNumber = parseInt(songNumberCol.attr("data-song-number"));
 
     if(songNumber !== currentlyPlayingSongNumber){
       songNumberCol.html(playButtonTemplate);
@@ -40,7 +42,7 @@ var createSongRow = function(songNumber, songName, songLength) {
 
   var offHover = function(event) {
     var songNumberCol = $(this).find(".song-item-number");
-    var songNumber = songNumberCol.attr("data-song-number");
+    var songNumber = parseInt(songNumberCol.attr("data-song-number"));
 
     if(songNumber !== currentlyPlayingSongNumber){
       songNumberCol.html(songNumber);
@@ -76,19 +78,77 @@ var setCurrentAlbum = function(album) {
   }
 };
 
+var trackIndex = function(album, song) {
+  return album.songs.indexOf(song);
+};
+
+var nextSong = function() {
+  var currentSongIndex = trackIndex(currentAlbum, currentSongFromAlbum);
+  currentSongIndex++;
+
+  if(currentSongIndex >= currentAlbum.songs.length) {
+    currentSongIndex = 0;
+  }
+
+  var lastSongNumber = currentlyPlayingSongNumber;
+
+  currentlyPlayingSongNumber = currentSongIndex + 1;
+  currentSongFromAlbum = currentAlbum.songs[currentSongIndex];
+
+  updatePlayerBarSong();
+
+  var $nextSongNumberCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+  var $lastSongNumberCell = $('.song-item-number[data-song-number="' + lastSongNumber + '"]');
+
+  $nextSongNumberCell.html(pauseButtonTemplate);
+  $lastSongNumberCell.html(lastSongNumber);
+};
+
+var previousSong = function() {
+  var currentSongIndex = trackIndex(currentAlbum, currentSongFromAlbum);
+  currentSongIndex--;
+
+  if(currentSongIndex < 0) {
+    currentSongIndex = currentAlbum.songs.length - 1;
+  }
+
+  var lastSongNumber = currentlyPlayingSongNumber;
+
+  currentlyPlayingSongNumber = currentSongIndex + 1;
+  currentSongFromAlbum = currentAlbum.songs[currentSongIndex];
+
+  updatePlayerBarSong();
+
+  $('.main-controls .play-pause').html(playerBarPauseButton);
+
+  var $previousSongNumberCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+  var $lastSongNumberCell = $('.song-item-number[data-song-number="' + lastSongNumber + '"]');
+
+  $previousSongNumberCell.html(pauseButtonTemplate);
+  $lastSongNumberCell.html(lastSongNumber);
+};
+
 var updatePlayerBarSong = function() {
     $('.currently-playing .song-name').text(currentSongFromAlbum.title);
     $('.currently-playing .artist-name').text(currentAlbum.artist);
     $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.title + " - " + currentAlbum.artist);
+    $('.main-controls .play-pause').html(playerBarPauseButton);
 };
 
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+var playerBarPlayButton = '<span class="ion-play"></span>';
+var playerBarPauseButton = '<span class="ion-pause"></span>';
 //store state of playing songs
 var currentAlbum = null; //store current album info
 var currentlyPlayingSongNumber = null; //stores current song number
 var currentSongFromAlbum = null; //stores currently playing song object from the songs array in fixtures.js
 
+var $previousButton = $('.main-controls .previous');
+var $nextButton = $('.main-controls .next');
+
 $(document).ready(function() {
   setCurrentAlbum(albumPicasso); //is this where the album function is now being told to use the albumPicasso object for the fuction(album) code?
+  $previousButton.click(previousSong);
+  $nextButton.click(nextSong);
 });
