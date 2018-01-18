262d59821e0589638fa7d71136d41f79026ce180
Aaron M Green <txhc010888@gmail.com>
2018-01-18 16:44:00 -0600

    corrected setVolume call on line 62

--- scripts/album.js
+++ scripts/album.js
@@ -58,7 +58,7 @@ var createSongRow = function(songNumber, songName, songLength) {
       var $volumeFill = $('.volume .fill');
       var $volumeThumb = $('.volume .thumb');
       $volumeFill.width(currentVolume = '%');
-      $volumeThumb.css({left: currentVolume + '%'});
+      $volumeThumb.css({left: currentVolume = '%'});
 
     } else if(currentlyPlayingSongNumber === songNumber){
       //Switch from Pause -> Play button to pause currently playing song.
